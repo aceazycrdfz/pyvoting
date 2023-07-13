b9d5221e4531e1549d02e1d0daa7b8a8db981488
aceazycrdfz <zycrdfz@gmail.com>
2023-07-13 14:53:35 +0800

    StandarizedScoreVoting Implemented!

--- main.py
+++ main.py
@@ -10,16 +10,25 @@ from RankedChoiceVoting import RankedChoiceVoting
 from TierListVoting import TierListVoting
 from TieredPopularityVoting import TieredPopularityVoting
 from NormalizedScoreVoting import NormalizedScoreVoting
+from StandardizedScoreVoting import StandardizedScoreVoting
 
 if __name__ == "__main__":
     
     candidates = ["Trump","Pence","Biden"]
-    election = NormalizedScoreVoting(candidates, try_handle_invalid=True)
+    election = StandardizedScoreVoting(candidates, try_handle_invalid=True)
     #print(election.ImportBallots("ballot.xlsx"))
     
     print(election.AddBallot(pd.Series({"Trump":1,
-                                        "Pence":4,
-                                        "Biden":5})))
+                                        "Pence":2,
+                                        "Biden":3})))
+    
+    print(election.AddBallot(pd.Series({"Trump":2,
+                                        "Pence":3,
+                                        "Biden":1})))
+    
+    print(election.AddBallot(pd.Series({"Trump":3,
+                                        "Pence":1,
+                                        "Biden":3})))
     
     print(election.RunElection())
     print(election.ExportBallots("ballot.xlsx"))
